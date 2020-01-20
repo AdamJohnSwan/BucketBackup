1d1f05e655a5e8598142e8b7a018bd2666fe00d7
John Labod <no-reply@labod.co>
2020-01-20 14:23:53 -0500

    process form to update settings and add cron job

--- src/usr/local/emhttp/plugins/bucketbackup/include/api.php
+++ src/usr/local/emhttp/plugins/bucketbackup/include/api.php
@@ -1,5 +1,6 @@
 <?php
 
+$plugin = "bucketbackup";
 
 function console_log($output, $with_script_tags = true) {
     $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
@@ -9,45 +10,32 @@ function console_log($output, $with_script_tags = true) {
     echo $js_code;
 }
 
-if (count($_POST)) {
-	if ($_POST['#apply'] == "Apply") {
-		$settingsObj = new \stdClass();
-		$settingsObj->app_id = $_POST["api_id"];
-		$settingsObj->app_key = $_POST["api_key"];
-		$settingsObj->backup_interval = $_POST["backup_interval"];
-		$settingsObj->backup_location = $_POST["backup_location"];
-		$settingsObj->bucket_type = $_POST["bucket_type"];
-		$settingsObj->encryption_password = $_POST["encryption_password"];
-		
-		$settingsJson = json_encode($settingsObj);
-		
-		console_log($settingsJson);
-		
-		$settings = fopen("settings.config", "w") or die("Unable to open file!");
-		fwrite($settings, $settingsJson);
-		fclose($settings);
-		
-		//Update the cron with the backup schedule
-		update_cron($_POST["backup_interval"]);
-	}
-}
 
 function update_cron($interval) {
-	if ($interval == "daily") {
-		$cronstring = "0 4 * * *";
-	}
-	if ($interval == "weekly") {
-		$cronstring = "0 4 * * 1";
-	}
-	if ($interval == "monthly") {
-		$cronstring = "0 4 1 * *";
-	}
-	$cronstring = "/usr/local/emhttp/plugins/bucketbackup/include/create_backup " . $cronstring;
-	shell_exec("mkdir -p /boot/config/plugins/dynamix");
-	shell_exec("touch bucketbackup.cron");
-	shell_exec("echo Backup job\n" . $cronstring . " > /tmp/output.txt" . "\n");
-	
-	
+		global $plugin;
+        $cronstring = "";
+        if ($interval == "daily") {
+                $cronstring = "0 4 * * *";
+        }
+        if ($interval == "weekly") {
+                $cronstring = "0 4 * * 1";
+        }
+        if ($interval == "monthly") {
+                $cronstring = "0 4 1 * *";
+        }
+        if ($cronstring != "") {
+                shell_exec("mkdir -p /boot/config/plugins/dynamix");
+                shell_exec("touch /boot/config/plugins/dynamix/{$plugin}.cron");
+                shell_exec("echo 'Backup job\n{$cronstring} /usr/local/emhttp/plugins/{$plugin}/include/create_backup\n' > /boot/config/plugins/dynamix/{$plugin}.cron");
+                shell_exec("update_cron");
+        }
+}
+
+function update_settings($settingsJson) {
+		global $plugin;
+		$settings = fopen("/usr/local/emhttp/plugins/{$plugin}/settings.config", "w") or die("Unable to open file!");
+		fwrite($settings, $settingsJson);
+		fclose($settings);
 }
 
 ?>
