3041962a9e553db47c31edd58edde3c87d533433
John Labod <johnlabod@noreply.gitlab.com>
2020-02-04 11:52:29 -0500

    let the user know if restoring the backup failed because the form isn't completely filled out

--- src/usr/local/emhttp/plugins/bucketbackup/include/restore_backup.php
+++ src/usr/local/emhttp/plugins/bucketbackup/include/restore_backup.php
@@ -1,33 +1,49 @@
 <?php
 
-// Send a response back to the user and continue processing the backup
-ignore_user_abort(true);
-set_time_limit(0);
-
-ob_start();
-echo "Restore started";
-header('Connection: close');
-header('Content-Length: '.ob_get_length());
-ob_end_flush();
-ob_flush();
-flush();
-
 if ($_POST['restore'] == "Restore") {
+	// Send a response back to the user and continue processing the backup
+	ignore_user_abort(true);
+	set_time_limit(0);
+
+	ob_start();
+	$restore_obj = new \stdClass();
+	$restore_obj->password = $_POST["encryption_password_restore"];
+	$restore_obj->bucket_info = $_POST["bucket_to_restore"];
+	$restore_obj->location = $_POST["restore_location"];
+	$restore_obj->api_id = $_POST["api_id_restore"];
+	$restore_obj->api_key = $_POST["api_key_restore"];
+	
+	// Make sure the form is all filled out
+	$form_complete = true;
+	foreach ($restore_obj as $key => $value) {
+		if($value == null) {
+			$form_complete = false;
+		}
+	}
+	
+	if(!$form_complete) {
+	  echo "Please complete the entire restore form";
+	  http_response_code(400);
+	  return;
+	} else {
+		http_response_code(200);
+		echo "Restore Started";
+	}
+	
+	header('Connection: close');
+	header('Content-Length: '.ob_get_length());
+	ob_end_flush();
+	ob_flush();
+	flush();
 	
 	//delete the old log file
 	if(file_exists("/tmp/bucketbackup/restore.log")) {
 		unlink("/tmp/bucketbackup/restore.log");
 	}
-	
-	$password = $_POST["encryption_password_restore"];
-	$bucket_info = $_POST["bucket_to_restore"];
-	$location = $_POST["restore_location"];
-	$api_id = $_POST["api_id_restore"];
-	$api_key = $_POST["api_key_restore"];
-	
+
 	write_to_log('Restore started at ' . date(DATE_RFC2822));
 	// get the auth token
-	$credentials = base64_encode($api_id . ":" . $api_key);
+	$credentials = base64_encode($restore_obj->api_id . ":" . $restore_obj->api_key);
 	$authurl = "https://api.backblazeb2.com/b2api/v2/b2_authorize_account";
 
 	$ch = curl_init($authurl);
@@ -73,7 +89,7 @@ if ($_POST['restore'] == "Restore") {
 	}
 	write_to_log("Found bucketbackup's bucket");
 	//Create temporary folder to house the downloads
-	$download_dir = $location . "bucketbackup_download_dir";
+	$download_dir = $restore_obj->location . "bucketbackup_download_dir";
 	if(file_exists($download_dir)) {
 		exec("rm -r $download_dir");
 	}
@@ -97,7 +113,7 @@ if ($_POST['restore'] == "Restore") {
 		$json = json_decode($list_files_response, true);
 		$files = $json['files'];
 		foreach($files as $key => $file) {
-			if($file["fileInfo"]["backup_job"] == $bucket_info) {
+			if($file["fileInfo"]["backup_job"] == $restore_obj->bucket_info) {
 				//Download the file
 				$downloadurl = $apiurl . "b2_download_file_by_id?fileId=" . $file["fileId"];
 				$destination = $download_dir . "/" . $file["fileName"];
@@ -119,12 +135,11 @@ if ($_POST['restore'] == "Restore") {
 					//Now to decrypt the file
 					$gzfile = str_replace(".dat", "", $file["fileName"]);
 					write_to_log("Decrypting...");
-					$message = 
-					write_to_log(shell_exec("openssl enc -aes-256-cbc -pbkdf2 -d -pass pass:{$password} < {$download_dir}/{$file["fileName"]} > {$download_dir}/{$gzfile}"));
+					write_to_log(shell_exec("openssl enc -aes-256-cbc -pbkdf2 -d -pass pass:{$restore_obj->password} < {$download_dir}/{$file["fileName"]} > {$download_dir}/{$gzfile}"));
 					//Remove the encrypted file
 					write_to_log(shell_exec("rm {$download_dir}/{$file["fileName"]}"));
 					//Unzip and untar the file
-					write_to_log(shell_exec("tar -xzf {$download_dir}/{$gzfile} -C {$location}"));
+					write_to_log(shell_exec("tar -xzf {$download_dir}/{$gzfile} -C {$restore_obj->location}"));
 					//Remove the tar file
 					write_to_log(shell_exec("rm {$download_dir}/{$gzfile}"));
 				} else {
