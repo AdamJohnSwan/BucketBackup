8f9c92efa74cb148ed5e2d28c32d9c851000bd07
Console and webGui login account <John>
2020-02-25 11:06:25 -0500

    removed tmp dir when uninstalling plugin / added plugin name to log.php so it can succesfully find the log files

--- src/usr/local/emhttp/plugins/bucketbackup/include/log.php
+++ src/usr/local/emhttp/plugins/bucketbackup/include/log.php
@@ -2,6 +2,7 @@
 
 
 if (!is_null($_POST['logtype'])) {
+	$plugin = "bucketbackup";
 	$logtype = $_POST['logtype'];
 	$logcontents = file_get_contents("/tmp/{$plugin}/{$logtype}.log");
 	echo nl2br($logcontents);
